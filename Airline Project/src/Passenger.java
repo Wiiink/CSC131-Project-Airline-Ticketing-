5b945a6ce5e3e901fe1f3759c8d64280219301b6
kdosa <kdosa@DESKTOP-MSNNF5U>
2021-11-24 15:57:16 -0800

    on the same path

--- Airline Project/src/Passenger.java
+++ Airline Project/src/Passenger.java
@@ -1,4 +1,29 @@
 
 public class Passenger {
 
+	private String name;
+	private int id;
+	
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public int getId() 
+	{
+		return id;
+	}
+	
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	
+
 }
